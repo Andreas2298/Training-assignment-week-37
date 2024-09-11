1aa1d6a9e8b728f8482b6a1f508d354ddde4cbc2
Andreas2298 <andre-as13@hotmail.no>
2024-09-11 14:26:48 +0200

    add assignment week 37.js

---
+++ Assignments week 37.js
@@ -0,0 +1,142 @@
+/*
+    Hi.
+    This is a set of exercises
+    The idea is to practice a few things at a time.
+    You do this by entering your answer after a task is given (see the example)
+
+    DO NOT change any provided code unless the task specifically tells you to.
+*/
+
+/*
+    Task: Example
+    Write code to print all the names in the list, one name per line
+*/
+console.log("Task: Example");
+const people = ["Tony", "Christian", "Håkon"];
+
+for (let index = 0; index < people.length; index++) {
+  let person = people[index];
+  console.log(person);
+}
+
+/*
+    Task: A
+    Write code that prints the sentence "Debugging is like being a detective in a crime drama where you are also the murderer" 
+    100 times. 
+    Remember we use console.log() to print things out.
+*/
+
+console.log("Task: A");
+
+const sentence =
+  "Debugging is like being a detective in a crime drama where you are also the murderer.";
+
+for (let index = 0; index < 100; index++) {
+  console.log(sentence);
+}
+
+/*
+    Task: B
+    The following code is incorrect, can you fix the errors?
+    This one isn't that simple, there are syntax errors and logical errors.
+    Try to think about what we are trying to achieve.
+*/
+console.log("Task: B");
+
+const max = 99;
+
+for (let index = max; index > 0; index--);
+{
+  console.log(index + " bottles of soda on the shelf");
+
+  console.log(
+    "Take one down, pass it around." +
+      (index - 1) +
+      " bottles of soda on the shelf"
+  );
+  console.log("");
+}
+
+console.log("No more bottles of soda on the shelf");
+
+/*
+    Task: C
+    Declare a variable for gravity, a variable for pi, and a variable for the number of people in a room. 
+*/
+console.log("Task: C");
+
+const gravity = 9.80665;
+console.log(gravity);
+
+const PI = 3.14159;
+console.log(PI);
+
+const peopleInRoom = 40;
+console.log(peopleInRoom);
+
+/*
+    Task: D
+    Create a function that adds two numbers, the function should be named add
+*/
+console.log("Task: D");
+
+// write your function here.
+
+function add(addendA, addendB) {
+  return addendA + addendB;
+}
+
+console.log("3 + 6 = " + add(3, 6));
+console.log("5673 + 234 = " + add(5673, 234));
+
+/*
+    Task: E
+    Use the variable people from the example and print the names in reverse order (using a loop)
+    NB: You should not change people, just use its contents.
+*/
+console.log("Task: E");
+
+for (let index = people.length - 1; index >= 0; index--) {
+  let person = people[index];
+  console.log(person);
+}
+
+/*
+    Task: F
+    Declare a variable for a list of phone numbers. Your list should have at least 3 numbers. 
+*/
+
+console.log("Task: F");
+
+const phoneNumbers = [11111111, 22222222, 33333333, 44444444, 55555555];
+
+console.log(phoneNumbers);
+
+/*
+    Task: G
+    Print only the last phone number in your list from Task F
+*/
+console.log("Task: G");
+
+for (let index = 4; index >= 0; index--) {
+  if (index == 4) {
+    console.log(phoneNumbers[index]);
+  }
+}
+
+/*
+    Task: H
+    Print only the last names of the people in the list, using a loop.
+*/
+console.log("Task: H");
+
+let peopleNames = [
+  ["Christian", "Simonsen"],
+  ["Tony", "Bergholtz"],
+];
+
+for (let index = 0; index < peopleNames.length; index++) {
+  let lastName = peopleNames[index][1];
+
+  console.log(lastName);
+}
